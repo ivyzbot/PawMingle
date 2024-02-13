8cabb17a5f10f849559921834903729e5a01675e
ivyzbot <ivyxiao83@gmail.com>
2024-02-14 00:08:40 +0800

    setup fe and be scaffolding

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <div>Landing Page</div>
+      <Button variant="contained">Test</Button>
+    </>
+  );
+}
+
+export default App;
