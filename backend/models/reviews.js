1786a769e726f2cdf8a36288d2f968927fa702dc
ivyzbot <ivyxiao83@gmail.com>
2024-02-24 22:28:05 +0800

    complete side section data query for be

--- backend/models/reviews.js
+++ backend/models/reviews.js
@@ -1,9 +1,39 @@
+const mongoose = require('mongoose');
 const reviewsDao = require('../daos/reviews');
 
-module.exports = { createReview };
+module.exports = { createReview, getReviewCount };
 
 async function createReview(body) {
   const newReview = await reviewsDao.create(body);
   // console.log('Create new review: ', newReview);
   return { success: true, data: newReview };
+}
+
+async function getReviewCount(userID) {
+  const reviewsHirer = await reviewsDao.aggregate([
+    {
+      $match: {
+        receiverID: new mongoose.Types.ObjectId(userID),
+        reviewType: 'Hirer',
+      },
+    },
+    { $group: { _id: '$receiverID', avg: { $avg: '$score' } } },
+  ]);
+
+  const reviewsProvider = await reviewsDao.aggregate([
+    {
+      $match: {
+        receiverID: new mongoose.Types.ObjectId(userID),
+        reviewType: 'Provider',
+      },
+    },
+    { $group: { _id: '$receiverID', avg: { $avg: '$score' } } },
+  ]);
+
+  const reviewsCount = {
+    reviewsHirer: reviewsHirer,
+    reviewsProvider: reviewsProvider,
+  };
+  // console.log('Review Count: ', reviewsCount);
+  return { success: true, data: reviewsCount };
 }
