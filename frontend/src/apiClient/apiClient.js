54ac1847363bb3bea9bf8c08251297726711a51a
ivyzbot <ivyxiao83@gmail.com>
2024-02-29 00:49:39 +0800

    fixing base url

--- frontend/src/apiClient/apiClient.js
+++ frontend/src/apiClient/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: import.meta.env.DEV
     ? 'http://localhost:3000/'
-    : 'https://pawmingle-backend.onrender.com',
+    : 'https://pawmingle-backend.onrender.com/',
   headers: {
     'Content-type': 'application/json',
   },
