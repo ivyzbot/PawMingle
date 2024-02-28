f4acb49e14a2f73629aaee2f3ce68dceef8f7546
ivyzbot <ivyxiao83@gmail.com>
2024-02-29 00:37:54 +0800

    update base url

--- frontend/src/apiClient/apiClient.js
+++ frontend/src/apiClient/apiClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:3000/' : '/',
+  baseURL: import.meta.env.DEV
+    ? 'http://localhost:3000/'
+    : 'https://pawmingle-backend.onrender.com',
   headers: {
     'Content-type': 'application/json',
   },
